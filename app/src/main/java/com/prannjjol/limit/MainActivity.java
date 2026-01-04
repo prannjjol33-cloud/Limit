9232f9b5dc0dac513f3cba5e17ee97ca47d266a6
Prannjjol Pratyussh <prannjjol33@gmail.com>
2026-01-05 00:23:14 +0530

    Created 4 screens.

---
+++ app/src/main/java/com/prannjjol/limit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.prannjjol.limit;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Toast;
+import android.widget.ImageButton;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        setupBottomNavigation();
+
+        ImageButton btnRefresh = findViewById(R.id.refreshButton);
+        btnRefresh.setOnClickListener(v -> Toast.makeText(this, "Refreshing expenses…", Toast.LENGTH_SHORT).show());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboardRoot), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
